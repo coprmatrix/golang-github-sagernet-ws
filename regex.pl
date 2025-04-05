d26741ace5a42eaa6a0abe53203dce97aef8c5d0
u <u>
2025-04-05 22:12:43 +0300

    update

--- regex.pl
+++ regex.pl
@@ -1,5 +1,5 @@
-s~%global\s+goipath\s+(.*)/(.*)~%global goihead \2\n%global goipath \1/%{goihead}~g;
-s~(Version:)\s*([^0-9\.]*)([0-9\.]*)([^\s]*)\s*~\1  \3\n%define oldver \2\3\4~g;
-s~(^%gometa.*)~%{?!tag:%{?!commit:%global tag v%{oldver}}}\n\1~g;
+s~%global\s+goipath\s+(.*)/(.*)~%global goihead \2\n%global goipath \1/%{goihead}\n~g;
+s~(Version:)\s*([^0-9\.]*)([0-9\.]*)([^\s]*)\s*~\1  \3\n%define oldver \2\3\4\n~g;
+s~(^%gometa.*)~%{?!tag:%{?!commit:%global tag v%{oldver}}}\n\1\n~g;
 s~%gocheck~~g;
 s~Source:.*~%define stag %{?tag}%{?!tag:%commit}\nSource: https://%{goipath}/archive/%{stag}/%{goihead}-%{stag}.tar.gz~g;
